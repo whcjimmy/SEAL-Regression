5ddd6395e3cfa1f22b97b269b9afa3a39c7a0966
Jimmy Wang <whcjimmy@cht.com.tw>
2021-03-02 14:22:14 +0800

    C++ training code

---
+++ klr.cpp
@@ -0,0 +1,207 @@
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <valarray>
+#include "util.cpp"
+
+using namespace std;
+
+void test_accuracy(vector<double> weights, vector<vector<double>> testing_x, vector<double> testing_y) {
+    double accuracy = 0.0, tmp;
+    int testing_rows = testing_x.size();
+    for(int i = 0; i < testing_rows; i++) {
+        tmp = vector_dot_product(weights, testing_x[i]);
+        if(tmp >= 0) {
+            tmp = 1;
+        } else {
+            tmp = -1;
+        }
+
+        if(tmp == testing_y[i]) accuracy += 1;
+    }
+    cout << "accuracy " << accuracy / testing_rows << endl;
+}
+
+
+int main()
+{
+    // read file
+    // string filename = "pulsar_stars.csv";
+    // string filename = "./python/datasets/Heart-Disease-Machine-Learning/data.csv";
+    string filename = "./python/datasets/breast_cancer/data.csv";
+    vector<vector<string>> s_matrix = CSVtoMatrix(filename);
+    vector<vector<double>> f_matrix = stringTodoubleMatrix(s_matrix);
+
+    // Init features, labels and weights
+    int total_rows = f_matrix.size();
+    cout << "\nNumber of rows  = " << total_rows << endl;
+    int total_cols = f_matrix[0].size() - 1;
+    cout << "\nNumber of cols  = " << total_cols << endl;
+
+    vector<vector<double>> features(total_rows, vector<double>(total_cols));
+    // Init labels (rows of f_matrix)
+    vector<double> labels(total_rows);
+
+    int training_rows = 364;
+    int testing_rows = total_rows - training_rows;
+    vector<vector<double>> training_x(training_rows, vector<double>(total_cols));
+    vector<vector<double>> testing_x(testing_rows, vector<double>(total_cols));
+    vector<double> training_y(training_rows);
+    vector<double> testing_y(testing_rows);
+
+    // Fill the features matrix and labels vector
+    for (int i = 0; i < total_rows; i++) {
+        for (int j = 0; j < total_cols; j++) {
+            if(i < training_rows) {
+                training_x[i][j] = f_matrix[i][j];
+            } else {
+                testing_x[i - training_rows][j] = f_matrix[i][j];
+            }
+        }
+        if(i < training_rows) {
+            training_y[i] = f_matrix[i][total_cols];
+        } else {
+            testing_y[i - training_rows] = f_matrix[i][total_cols];
+        }
+    }
+
+    // Init weights
+    // Init weight vector with zeros (cols of features)
+    vector<double> weights(training_rows);
+    for (int i = 0; i < training_rows; i++) {
+        weights[i] = 0;
+        // weights[i] = RandomFloat(-0.1, 0.1);
+    }
+
+    // Polynomial Simulated Sigmoid Function
+    double poly_deg = 3;
+    // double poly_deg = 7;
+
+    vector<double> coeffs = {0.50091, 0.19832, -0.00018, -0.0044};
+    // vector<double> coeffs = {0.50054, 0.19688, -0.00014, -0.00544, 0.000005, 0.000075, -0.00000004, -0.0000003};
+
+
+    // Parameters Settings
+    int col_A = 15;
+    int col_B = total_cols - col_A;
+
+    double learning_rate = 0.01;
+    int iter_times = 20;
+    double gamma  = 0.1;
+    double lambda = 0.01;
+    int poly_kernel_deg = 3;
+
+    // KERNEL
+    vector<vector<double>> training_kernel(training_rows, vector<double>(training_rows));
+    vector<vector<double>> testing_kernel(testing_rows, vector<double>(training_rows));
+
+    cout << "---------- LINEAR KERNEL ----------" <<endl;
+    // Training Kernel
+    for(int i = 0; i < training_rows; i++) {
+        for(int j = 0; j < training_rows; j++) { 
+            for(int k = 0; k < total_cols; k++) { 
+                training_kernel[i][j] += training_x[i][k] * training_x[j][k];
+
+            }
+        }
+    }
+    // Testing Kernel
+    for(int i = 0; i < testing_rows; i++) {
+        for (int j = 0; j < training_rows; j++) { 
+            for(int k = 0; k < total_cols; k++) { 
+                testing_kernel[i][j] += testing_x[i][k] * training_x[j][k];
+            }
+        }
+    }
+
+    /*
+    cout << "---------- POLYNOMIAL KERNEL ----------" <<endl;
+    // Training Kernel
+    for(int i = 0; i < training_rows; i++) {
+        for(int j = 0; j < training_rows; j++) { 
+            for(int k = 0; k < total_cols; k++) {
+                training_kernel[i][j] += training_x[i][k] * training_x[j][k];
+            }
+            training_kernel[i][j] = training_kernel[i][j] * gamma;
+            training_kernel[i][j] = 1 + training_kernel[i][j];
+            training_kernel[i][j] = pow(training_kernel[i][j], poly_kernel_deg);
+        }
+    }
+    // Testing Kernel
+    for(int i = 0; i < testing_rows; i++) {
+        for (int j = 0; j < training_rows; j++) { 
+            for(int k = 0; k < total_cols; k++) { 
+                testing_kernel[i][j] += testing_x[i][k] * training_x[j][k];
+            }
+            testing_kernel[i][j] = testing_kernel[i][j] * gamma;
+            testing_kernel[i][j] = 1 + testing_kernel[i][j];
+            testing_kernel[i][j] = pow(testing_kernel[i][j], poly_kernel_deg);
+        }
+    }
+
+    cout << "---------- RBF KERNEL ----------" <<endl;
+    // Training Kernel
+    for(int i = 0; i < training_rows; i++) {
+        for (int j = 0; j < training_rows; j++) { 
+            for(int k = 0; k < total_cols; k++) {
+                training_kernel[i][j] += pow(training_x[i][k] - training_x[j][k], 2);
+            }
+            training_kernel[i][j] = -1 * training_kernel[i][j] * gamma;
+            training_kernel[i][j] = 1 + training_kernel[i][j] + pow(training_kernel[i][j], 2) / 2;
+        }
+    }
+    // Testing Kernel
+    for(int i = 0; i < testing_rows; i++) {
+        for (int j = 0; j < training_rows; j++) { 
+            for(int k = 0; k < total_cols; k++) { 
+                testing_kernel[i][j] += pow(testing_x[i][k] - training_x[j][k], 2);
+            }
+            testing_kernel[i][j] = -1 * testing_kernel[i][j] * gamma;
+            testing_kernel[i][j] = 1 + testing_kernel[i][j] + pow(testing_kernel[i][j], 2) / 2;
+        }
+    }
+    */
+
+
+    // Calculate gradient descents in the plaintext domain
+    vector<double> delta_w(training_rows, 0.0);
+    vector<double> l2_reg(training_rows, 0.0);
+    double w_x, tmp;
+    for(int iter = 0; iter < iter_times; iter++) {
+        cout << "iter " << iter << endl;
+
+        fill(delta_w.begin(), delta_w.end(), 0);
+
+        for(int i = 0; i < training_rows; i++) {
+            w_x = vector_dot_product(weights, training_kernel[i]);
+            tmp = 0.0;
+
+            for(int j = 0; j <= poly_deg; j++) {
+                tmp = coeffs[j] * pow(-1 * training_y[i], j + 1);
+                tmp = tmp * pow(w_x, j);
+                for(int k = 0; k < training_rows; k++) {
+                    delta_w[k] += tmp * training_kernel[i][k];
+                }
+            }
+        }
+
+        l2_reg = linear_transformation(training_kernel, weights);
+        tmp = 2 * lambda / training_rows;
+
+        for(int i = 0; i < training_rows; i++) {
+            weights[i] = weights[i] - learning_rate * (l2_reg[i] * tmp + delta_w[i] / training_rows);
+        }
+
+        // Show results
+        for(int i = 0; i < 10; i++) {
+            cout << weights[i] << " ";
+        }
+        cout << endl;
+
+        test_accuracy(weights, testing_kernel, testing_y);
+    }
+
+    return 0;
+}
---
+++ lr.cpp
@@ -0,0 +1,128 @@
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <valarray>
+#include "util.cpp"
+
+using namespace std;
+
+void test_accuracy(vector<double> weights, vector<vector<double>> testing_x, vector<double> testing_y) {
+    double accuracy = 0.0, tmp;
+    int testing_rows = testing_x.size();
+    for(int i = 0; i < testing_rows; i++) {
+        tmp = vector_dot_product(weights, testing_x[i]);
+        if(tmp >= 0) {
+            tmp = 1;
+        } else {
+            tmp = -1;
+        }
+
+        if(tmp == testing_y[i]) accuracy += 1;
+    }
+    cout << "accuracy " << accuracy / testing_rows << endl;
+}
+
+
+int main()
+{
+    // read file
+    // string filename = "pulsar_stars.csv";
+    // string filename = "./python/datasets/Heart-Disease-Machine-Learning/data.csv";
+    string filename = "./python/datasets/breast_cancer/data.csv";
+    vector<vector<string>> s_matrix = CSVtoMatrix(filename);
+    vector<vector<double>> f_matrix = stringTodoubleMatrix(s_matrix);
+
+    // Init features, labels and weights
+    int total_rows = f_matrix.size();
+    cout << "\nNumber of rows  = " << total_rows << endl;
+    int total_cols = f_matrix[0].size() - 1;
+    cout << "\nNumber of cols  = " << total_cols << endl;
+
+    vector<vector<double>> features(total_rows, vector<double>(total_cols));
+    // Init labels (rows of f_matrix)
+    vector<double> labels(total_rows);
+    // Init weight vector with zeros (cols of features)
+    vector<double> weights(total_cols);
+
+    int training_rows = 364;
+    int testing_rows = total_rows - training_rows;
+    vector<vector<double>> training_x(training_rows, vector<double>(total_cols));
+    vector<vector<double>> testing_x(testing_rows, vector<double>(total_cols));
+    vector<double> training_y(training_rows);
+    vector<double> testing_y(testing_rows);
+
+    // Fill the features matrix and labels vector
+    for (int i = 0; i < total_rows; i++) {
+        for (int j = 0; j < total_cols; j++) {
+            if(i < training_rows) {
+                training_x[i][j] = f_matrix[i][j];
+            } else {
+                testing_x[i - training_rows][j] = f_matrix[i][j];
+            }
+        }
+        if(i < training_rows) {
+            training_y[i] = f_matrix[i][total_cols];
+        } else {
+            testing_y[i - training_rows] = f_matrix[i][total_cols];
+        }
+    }
+
+    // Init weights
+    for (int i = 0; i < total_cols; i++) {
+        weights[i] = 0;
+        // weights[i] = RandomFloat(-0.1, 0.1);
+    }
+
+    // Polynomial Simulated Sigmoid Function
+    double poly_deg = 3;
+    // double poly_deg = 7;
+
+    vector<double> coeffs = {0.50091, 0.19832, -0.00018, -0.0044};
+    // vector<double> coeffs = {0.50054, 0.19688, -0.00014, -0.00544, 0.000005, 0.000075, -0.00000004, -0.0000003};
+
+
+    // Parameters Settings
+    int col_A = 15;
+    int col_B = total_cols - col_A;
+
+    double learning_rate = 1;
+    int iter_times = 20;
+
+    // Calculate gradient descents in the plaintext domain
+    vector<double> delta_w(total_cols, 0.0);
+    double w_x, tmp;
+    for(int iter = 0; iter < iter_times; iter++) {
+        cout << "iter " << iter << endl;
+
+        fill(delta_w.begin(), delta_w.end(), 0);
+
+        for(int i = 0; i < training_rows; i++) {
+            w_x = vector_dot_product(weights, training_x[i]);
+            tmp = 0.0;
+
+            for(int j = 0; j <= poly_deg; j++) {
+                tmp = coeffs[j] * pow(-1 * training_y[i], j + 1);
+                tmp = tmp * pow(w_x, j);
+                for(int k = 0; k < total_cols; k++) {
+                    delta_w[k] += tmp * training_x[i][k];
+                }
+            }
+        }
+
+        for(int i = 0; i < total_cols; i++) {
+            weights[i] = weights[i] - learning_rate * delta_w[i] / training_rows;
+        }
+
+        // Show results
+        for(int i = 0; i < 10; i++) {
+            cout << weights[i] << " ";
+        }
+        cout << endl;
+
+        test_accuracy(weights, testing_x, testing_y);
+    }
+
+    return 0;
+}
